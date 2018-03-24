402c86a31c22f43391766a2251cf90edc5c4fad5
Gilary Brito <Gilary_FB@live.nl>
2018-03-24 12:54:39 +0100

    Pragrammingisfun chapter 4

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            string CharacterName = "Espen";
+
+            Console.WriteLine("Strangers");
+            Console.WriteLine("Welcome stranger, are you from around here? You do not look familiar!");
+            Console.WriteLine("Before we start our adventure you should tell me your name then stranger.");
+            Console.WriteLine("So what is your name stranger?");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine(CharacterName + ", what a great name for a stranger!");
+            Console.WriteLine("So let's get started " + CharacterName +".");
+            Console.Read();
+        }
+    }
+}
